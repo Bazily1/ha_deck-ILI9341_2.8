ee9e760a71ef908f056c049bf432386f938472fd
Bazily1 <68904094+Bazily1@users.noreply.github.com>
2024-08-19 00:27:25 +0300

    Update LGFX.h

--- components/hd_device_ILI9341/LGFX.h
+++ components/hd_device_ILI9341/LGFX.h
@@ -11,10 +11,16 @@ namespace hd_device {
 
 class LGFX : public lgfx::LGFX_Device
 {
-  lgfx::Panel_ST7796 _panel_instance;
-  lgfx::Bus_Parallel8 _bus_instance;
-  lgfx::Light_PWM _light_instance;
-  lgfx::Touch_FT5x06 _touch_instance;
+
+  lgfx::Panel_ILI9341 _panel_instance;
+  lgfx::Bus_SPI       _bus_instance;
+  lgfx::Light_PWM     _light_instance;
+  lgfx::Touch_XPT2046 _touch_instance;
+
+  //lgfx::Panel_ST7796 _panel_instance;
+  //lgfx::Bus_Parallel8 _bus_instance;
+  //lgfx::Light_PWM _light_instance;
+  //lgfx::Touch_FT5x06 _touch_instance;
 
 public:
   LGFX(void);
